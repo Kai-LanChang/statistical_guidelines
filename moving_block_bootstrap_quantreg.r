518fc9f1e581de223939c126cffc7910ee7f0b3c
KAI-LAN CHANG <thekylan@gmail.com>
2023-04-14 15:40:30 -0600

    Update moving_block_bootstrap_quantreg.r

--- moving_block_bootstrap_quantreg.r
+++ moving_block_bootstrap_quantreg.r
@@ -20,15 +20,15 @@ mbfun=function(formula,data,tau){
  nblocks=ceiling(n/b)
  blocks=lapply(seq_len(n-b+1), function(i) seq(i, i+b-1))
  bn=sample(1:length(blocks),nblocks,replace=T)
- sampdata=data[unlist(blocks[bn]), ]  
- mod=rq(formula, data=sampdata, tau=tau)
+ samp_data=data[unlist(blocks[bn]), ]  
+ mod=rq(formula, data=samp_data, tau=tau)
  coef(mod)
 }
 
 set.seed(2013)
 fit=coef(rq(yd~x, data=mlo, tau=0.5))*12 #intercept and slope
 op=t(replicate(1000, mbfun(formula=yd~x,data=mlo,tau=0.5)))
-fit_se=t(apply(op, 2, sd, na.rm=TRUE))*12 #MBB standard error for intercept and slope
+fit_se=apply(op, 2, sd, na.rm=TRUE)*12 #MBB standard error for intercept and slope
 fit_pv=2*pt(q=abs(fit/fit_se), df=nrow(mlo)-2, lower.tail=FALSE) #MBB p value for intercept and slop
 #multiple quantiles 
 #op=t(replicate(1000, mbfun(formula=yd~x,data=mlo,tau=seq(0.1,0.9,by=0.1))[2,]))
